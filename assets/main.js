91cf091590be6a613fdad8b7563dc442d9a222e2
k881-droid <k881@newschool.edu>
2026-04-01 04:22:31 -0400

    Inputting time related data
    Was able to add data json successfully

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,18 @@
+let renderItems = (data) => {
+    let containerEl = document.querySelector('#time-log')
+    data.forEach(item => {
+        let itemHtml = 
+        `  
+            <li>
+                <p>${item['Task']}</p>
+                <p>${item['Time taken']}</p>
+            </li>
+        `
+
+        containerEl.insertAdjacentHTML('beforeend', itemHtml)
+    })
+}
+
 fetch('assets/data.json')
     .then(response => response.json())
     .then(data => {
