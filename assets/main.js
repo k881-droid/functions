627c3ebb630e5330c7120a3a5767bbaf9538c34c
k881-droid <k881@newschool.edu>
2026-04-01 04:43:33 -0400

    Core function beginning
    Updated data.json with conditional

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,35 @@
 let renderItems = (data) => {
     let containerEl = document.querySelector('#time-log')
+    
     data.forEach(item => {
+        let timeTaken = item['Time taken']
+        let className = ''
+
+        if (timeTaken < 15) {
+            timeTaken = 'small'
+            className = 'small-task'
+        } else{
+            timeTaken = `${timeTaken} minutes`
+        }
+
+        // if (30 < timeTaken > 15) {
+        //     timeTaken = 'medium'
+        //     className = 'medium-task'
+        // } else{
+        //     timeTaken = `${timeTaken} minutes`
+        // }
+
+        //  if (timeTaken > 30) {
+        //     timeTaken = 'medium'
+        //     className = 'medium-task'
+        // } else{
+        //     timeTaken = `${timeTaken} minutes`
+        // }
+
+
         let itemHtml = 
         `  
-            <li>
+            <li class="${className}">
                 <p>${item['Task']}</p>
                 <p>${item['Time taken']}</p>
             </li>
